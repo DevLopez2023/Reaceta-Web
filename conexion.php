1f071ab2e66d224fceb2d7ba4cbfad91f88b28e4
Oscar Alexander <122540593+DevLopez2023@users.noreply.github.com>
2023-07-06 08:34:25 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 
 //Credenciales de la BD y Server
-$DB_USER = "postgres";
-$DB_HOST = "localhost";
-$DB_NAME = "eta";
-$DB_PASSWORD = "123";
+$DB_USER = "";
+$DB_HOST = "";
+$DB_NAME = "";
+$DB_PASSWORD = "";
 
 try{
     $conexion_pdo = new PDO("pgsql:host = $DB_HOST; dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
