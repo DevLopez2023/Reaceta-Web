0202c0727fea1c52fb46a7044cc3623524225bbb
Oscar Alexander <122540593+DevLopez2023@users.noreply.github.com>
2023-07-06 08:41:12 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,9 +5,10 @@ $DB_USER = "postgres";
 $DB_HOST = "containers-us-west-43.railway.app";
 $DB_NAME = "railway";
 $DB_PASSWORD = "u0eCMiHvRDadpEZtVKSr";
+$DB_PORT = "7935";
 
 try{
-    $conexion_pdo = new PDO("pgsql:host = $DB_HOST; dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
+    $conexion_pdo = new PDO("pgsql:host = $DB_HOST; port = $DB_PORT, dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
 }catch(PDOException $exp){
     echo 'Error de conexión a la BD: '. $exp;
 }
