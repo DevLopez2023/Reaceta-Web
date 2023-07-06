db4c488066e1a744a5d97a87c91d27e25e0f414f
Oscar Alexander <122540593+DevLopez2023@users.noreply.github.com>
2023-07-06 08:14:33 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,9 +5,10 @@ $DB_USER = "postgres";
 $DB_HOST = "localhost";
 $DB_NAME = "eta";
 $DB_PASSWORD = "123";
+$DB_PORT = 5432;
 
 try{
-    $conexion_pdo = new PDO("pgsql:host = $DB_HOST; dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
+    $conexion_pdo = new PDO("pgsql:$DB_PORT = $DB_HOST; dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
 }catch(PDOException $exp){
     echo 'Error de conexión a la BD: '. $exp;
 }
