4669779b2f956bbfa8d3481b17a006e0d04a421f
Oscar Alexander <122540593+DevLopez2023@users.noreply.github.com>
2023-07-06 07:49:43 -0500

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 
 //Credenciales de la BD y Server
-$usuario = "postgres";
-$host = "localhost";
-$bd = "eta";
-$pas = "123";
+$DB_USER = "postgres";
+$DB_HOST = "localhost";
+$DB_NAME = "eta";
+$DB_PASSWORD = "123";
 
 try{
-    $conexion_pdo = new PDO("pgsql:host = $host; dbname = $bd", $usuario, $pas);
+    $conexion_pdo = new PDO("pgsql:host = $DB_HOST; dbname = $DB_NAME", $DB_USER, $DB_PASSWORD);
 }catch(PDOException $exp){
     echo 'Error de conexión a la BD: '. $exp;
 }
