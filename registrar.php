501a94b62b7485121c05f224c2c6283a7cb1288c
Oscar Alexander <122540593+DevLopez2023@users.noreply.github.com>
2023-07-13 15:44:57 -0500

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -24,7 +24,7 @@ fecha_ini = '$fi',
 observaciones = '$obs'
 WHERE cod_materia = '$materia' AND cod_profesor = '$profesor';");
 
-$sentencia_d->pg_query();
+$sentencia_d->execute();
 
 if($sentencia_d == true){
     $_SESSION['registro'] = "Reactivos registrados";
